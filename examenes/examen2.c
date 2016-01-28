16ce3bc051d32c721664c59a402f3b80098354c0
Alberto Pickering <alberto911_@hotmail.com>
2016-01-28 10:46:31 -0600

    Examen 2

--- examenes/examen2.c
+++ examenes/examen2.c
@@ -4,6 +4,11 @@
 
 #define N 10
 
+typedef struct {
+	char* titulo;
+	int paginas;
+} libro;
+
 typedef void* (*pos_t)(void*, int);
 typedef void* (*next_t)(void*);
 typedef void (*print_t)(void*);
@@ -12,15 +17,22 @@ typedef void (*iterator_t)(void*, pos_t, pos_t, next_t, next_t, print_t, size_t,
 //void recorre(void* a, iterator_t it);
 void forwardIterator(void* a, next_t begin, pos_t end, next_t next, next_t prev, print_t print, size_t size, int total);
 void reverseIterator(void* a, next_t begin, pos_t end, next_t next, next_t prev, print_t print, size_t size, int total);
+void bidirectionalIterator(void* a, next_t begin, pos_t end, next_t next, next_t prev, print_t print, size_t size, int total);
 void* beginInt(void* p);
 void* endInt(void* p, int total);
 void* nextInt(void* p);
 void* prevInt(void* p);
 void printInt(void* p);
 
+void* beginLibro(void* p);
+void* endLibro(void* p, int total);
+void* nextLibro(void* p);
+void* prevLibro(void* p);
+void printLibro(void* p);
+
 int main() {
 	int* arreglo = (int*) malloc(N * sizeof(int)), *p;
-
+	
 	srand((int) time(NULL));
     
     for (p = arreglo; p < arreglo + N; ++p)
@@ -29,6 +41,9 @@ int main() {
 	forwardIterator(arreglo, beginInt, endInt, nextInt, prevInt, printInt, sizeof(int), N);
 	printf("\n");
 	reverseIterator(arreglo, beginInt, endInt, nextInt, prevInt, printInt, sizeof(int), N);
+	printf("\n");
+	bidirectionalIterator(arreglo, beginInt, endInt, nextInt, prevInt, printInt, sizeof(int), N);
+	printf("\n");
 	free(arreglo);
 }
 
@@ -54,6 +69,11 @@ void reverseIterator(void* a, next_t begin, pos_t end, next_t next, next_t prev,
 	} 
 }
 
+void bidirectionalIterator(void* a, next_t begin, pos_t end, next_t next, next_t prev, print_t print, size_t size, int total) {
+	forwardIterator(a, begin, end, next, prev, print, size, total);
+	reverseIterator(a, begin, end, next, prev, print, size, total);
+}
+
 void* beginInt(void* p) {
 	return p;
 }
@@ -72,4 +92,24 @@ void* prevInt(void* p) {
 
 void printInt(void* p) {
 	printf("%d ", *(int*) p);
+}
+
+void* beginLibro(void* p) {
+	return p;
+}
+
+void* endLibro(void* p, int total) {
+	return (libro*) p + total - 1;
+}
+
+void* nextLibro(void* p) {
+	return (libro*) p + 1;
+}
+
+void* prevLibro(void* p) {
+	return (libro*) p - 1;
+}
+
+void printLibro(void* p) {
+	printf("%s, %d páginas\n", ((libro*) p)->titulo, ((libro*) p)->paginas);
 }
